f0e6d3a1166b50cc046daa9006b8bbfbb5e82f5f
jackhowa <johncarverhoward@gmail.com>
2017-08-08 13:59:33 -0500

    add test comments to fulfill problem requirements on hacker rank

--- triplets.rb
+++ triplets.rb
@@ -4,9 +4,11 @@ def solve(a0, a1, a2, b0, b1, b2)
     aliceScore = 1 
     bobScore = 1 
 
+    # returns the two scores separated by space
     return aliceScore," ",bobScore
 end
 
+# need to add these for the test case 
 a0 = 5 
 a1 = 6 
 a2 = 7
