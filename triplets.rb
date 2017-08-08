6e985cd2c9d46b5b2be505d94f7d7ff95ad0d344
jackhowa <johncarverhoward@gmail.com>
2017-08-08 14:23:10 -0500

    add conditional within each with index loop for going through both alice and bob's arrays

--- triplets.rb
+++ triplets.rb
@@ -1,11 +1,24 @@
 #!/bin/ruby
 
 def solve(a0, a1, a2, b0, b1, b2)
-    aliceScore = 1 
-    bobScore = 1 
+  # setting up target variables 
+  aliceScores = [a0, a1, a2]
+  bobScores = [b0, b1, b2]
 
-    # returns the two scores separated by space
-    return aliceScore," ",bobScore
+  # output variables 
+  aliceFinal = 0 
+  bobFinal = 0
+
+  aliceScores.each_with_index do |aliceScore, i|
+    if aliceScore > bobScores[i]
+      aliceFinal += 1 
+    elsif aliceScore < bobScores[i]
+      bobFinal += 1
+    end
+  end 
+
+  # returns the two scores separated by a string space
+  return aliceFinal," ",bobFinal
 end
 
 # need to add these for the test case 
